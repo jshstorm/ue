01c0b2e16cc94f488280ac38003641e6d438a28e
storm <storm@clover.games>
2025-09-26 12:19:43 +0900

    anu data table removed

--- test/Plugins/AnuDataTable/Source/AnuDataTable/Private/DataTable/AnuDataTable.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DataTable/AnuDataTable.h"
-
-UAnuDataTable::UAnuDataTable(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
